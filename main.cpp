a5a2573ba3f1cfdf997ca6e8697012604fba002a
zeefu <hazedzephyr@gmail.com>
2023-01-14 15:22:36 -0800

    Test nop function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include "Bus.h"
 #include <SDL.h>
 
 int main(int argc, char* argv[]) {
+    Bus sm83_bus;
+    sm83_bus.cpu.nop();
+    int t = sm83_bus.ram[0xFFFF];
+    std::cout << t << std::endl;
+
     return 0;
 }
