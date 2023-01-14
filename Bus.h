d25500dfcd3b28dcb929e1c0086cf401513c2189
zeefu <hazedzephyr@gmail.com>
2023-01-14 15:22:19 -0800

    Update RAM initialization

--- Bus.h
+++ Bus.h
@@ -10,7 +10,9 @@ public:
     ~Bus();
     // Initialize a CPU object
     SM83 cpu;
-    std::array<uint8_t, 0xFFFF> ram;
+    // Initialize ram - Note need the {} for it to default initialize
+    std::array<uint8_t, 64 * 1024> ram{};
+    // Helper functions for reading and writing RAM
     void write(uint16_t addr, uint8_t data);
     uint8_t read(uint16_t addr, bool bReadOnly = false);
 };
