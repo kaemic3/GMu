a0ebc77c2173dbf3219ec4b3b42cd000a24aad21
zeefu <hazedzephyr@gmail.com>
2023-01-28 14:28:49 -0800

    WIP

--- SDL_Handler.cpp
+++ SDL_Handler.cpp
@@ -1,16 +1,73 @@
 #include "SDL_Handler.h"
+#include <cstdio>
 
 SDL_Handler::SDL_Handler() {
-
+    pWindow = nullptr;
+    pRenderer = nullptr;
 }
 
 SDL_Handler::~SDL_Handler() {
 
 }
 
-zText::zText() {
+bool SDL_Handler::init(const std::string &title, const int wScreen, const int hScreen) {
+    // Initialize SDL subsystems using guards instead of nested if
 
+    // Initialize SDL
+    if(SDL_Init(SDL_INIT_VIDEO < 0)) {
+        printf("Failed to initialize SDL! SDL Error: %s\n", SDL_GetError());
+        return false;
+    }
+    // Initialize SDL_ttf
+    if(TTF_Init() == -1) {
+        printf("Failed to initialize SDL_ttf! SDL Error: %s\n", TTF_GetError());
+    }
+    // Set texture filtering
+    if(!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
+        printf("Warning: Linear texture filtering is not enabled");
+    // Create window
+    pWindow = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, wScreen, hScreen, SDL_WINDOW_SHOWN);
+    if(pWindow == nullptr) {
+        printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+        return false;
+    }
+    // Create V-Synced renderer for our window
+    pRenderer = SDL_CreateRenderer(pWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (pRenderer == nullptr) {
+        printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+        return false;
+    }
+    // Set renderer color
+    SDL_SetRenderDrawColor(pRenderer, 0, 2, 107, 0xFF);
+    return true;
+}
+
+int SDL_Handler::pollEvent() {
+    return SDL_PollEvent(&eventHandler);
+}
+void SDL_Handler::clearScreen() {
+    SDL_SetRenderDrawColor(pRenderer, 0, 2, 107, 0xFF);
+    SDL_RenderClear(pRenderer);
+}
+void SDL_Handler::renderPresent() {
+    SDL_RenderPresent(pRenderer);
+}
+zText::zText(SDL_Handler *curHandler, std::string text, int x, int y, std::string color, std::string fontType, int fontSize) {
+    pHandler = curHandler; tCurText = text; tX = x; tY = y;
 }
 zText::~zText() {
 
 }
+// Return true if color is in the ColorMap
+// Return False if the color is not in the ColorMap
+bool zText::setColor(std::string color) {
+    // Need to find the text color in our ColorMap
+}
+
+std::map<std::string, std::string> zText::sFontMap = {
+    {"Amstrad CPC", "../Fonts/amstrad_cpc464.ttf"},
+    {"Fira Code", "../Fonts/FiraCode.ttf"}
+};
+std::map<std::string, SDL_Color> zText::sColorMap = {
+        { "Yellow", {243, 243, 13} }
+};
--- SDL_Handler.h
+++ SDL_Handler.h
@@ -2,62 +2,87 @@
 #define GMU_SDL_HANDLER_H
 
 #include <string>
+#include <map>
 #include <SDL.h>
 #include <SDL_ttf.h>
 
-class zText{
-    // Initialize variables
-    zText();
-    // Deallocate memory
-    ~zText();
-
-    // Loads image at specified path
-    bool loadFromFiles(const std::string &path);
-    // Creates image from font string
-    bool loadFromRenderedText(std::string textureText, SDL_Color textColor);
-    // Deallocates texture
-    void free();
-    // Set color modulation
-    void setColor(uint8_t red, uint8_t green, uint8_t blue);
-    // Set blending
-    void setBlendMode(SDL_BlendMode blending);
-    // Set alpha modulation
-    void setAlpha(uint8_t alpha);
-    // Renders texture at given point
-    void render(int x, int y, SDL_Rect *clip = nullptr, double angle = 0.0, SDL_Point *center = nullptr, SDL_RendererFlip flip = SDL_FLIP_NONE);
-    // Get image dimensions
-    int getWidth();
-    int getHeight();
-    // Actual hardware texture
-    SDL_Texture *mTexture;
-    // Image dimensions
-    int mWidth;
-    int mHeight;
-};
 
 class SDL_Handler {
-    // Need access to zText object's private members
-    friend class zText;
 public:
     // Constructor
     SDL_Handler();
     // Destructor
     ~SDL_Handler();
-
+    // Starts up SDL and creates window
+    bool init(const std::string &title, const int wScreen, const int hScreen);
+    // Frees media
+    void close();
+    // Event handler - Public for now
+    SDL_Event eventHandler;
+    // Checks to see if there are any events on queue
+    int pollEvent();
+    // Clear the screen
+    void clearScreen();
+    // Update screen
+    void renderPresent();
 private:
     SDL_Window *pWindow;
     SDL_Renderer *pRenderer;
-    TTF_Font *pFont;
-    zText textTexture;
-    // Starts up SDL and creates window
-    bool init();
-    // Loads media
-    bool loadMedia();
-    // Frees media
-    void close();
+
 
 };
 
 
+class zText{
+public:
+    // Initialize variables
+    zText(SDL_Handler *curHandler, std::string text, int x, int y, std::string color, std::string fontType, int fontSize);
+    // Deallocate memory
+    ~zText();
+    // Update text
+    bool updateText(std::string nText);
+    // Creates image from font string - this should be a helper function
+    bool generateNewTexture(std::string textureText);
+    // Deallocates texture
+    void free();
+    // Renders texture at given point
+    void render(int x, int y, SDL_Rect *clip = nullptr, double angle = 0.0, SDL_Point *center = nullptr, SDL_RendererFlip flip = SDL_FLIP_NONE);
+    // Load fonts list, may only be used in constructor???
+    bool loadMedia();
+    // Get image dimensions
+    int getWidth();
+    int getHeight();
+    // Get current string
+    std::string getString();
+    // Set color
+    bool setColor(std::string color);
+
+private:
+    // Current SDL_Handler
+    SDL_Handler *pHandler;
+    // Image dimensions
+    int tWidth;
+    int tHeight;
+    // Text position
+    int tX;
+    int tY;
+
+    // Current font
+    TTF_Font *tFont;
+    // Font size
+    uint8_t tFontSize;
+    // Font color
+    SDL_Color tFontColor;
+
+    // Current text as std::string
+    std::string tCurText;
+    // Text texture
+    SDL_Texture *tTexture;
+    // Available fonts - maybe make this a std::vector<std::pair<std::string, std::string>> ??
+    // Also static as there is no need for duplicates here, need to define in main file.
+    static std::map<std::string, std::string> sFontMap;
+    // Color map
+    static std::map<std::string, SDL_Color> sColorMap;
+};
 
 #endif //GMU_SDL_HANDLER_H
