cad4f7ad6cdd29f65ec431b69f6a531335f4a575
zeefu <hazedzephyr@gmail.com>
2023-01-30 18:29:21 -0800

    Add keyboard input handling

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <sstream>
 #include "Bus.h"
 #include "SDL_Handler.h"
 
@@ -11,7 +12,7 @@ const int REGISTER_X_OFFSET = 1040;
 // Used to convert decimal value to hex string
 std::string toHex(int num) {
     std::stringstream stream;
-    stream << std::hex << num;
+    stream << std::setfill('0') << std::setw(sizeof (uint16_t) * 2) << std::hex << num;
     return stream.str();
 }
 // Need to use setup SDL to test instructions
@@ -28,22 +29,39 @@ int main(int argc, char* argv[]) {
     // Initialize SDL_Handler
     if(!wSDLMain.init("GMu Debug", SCREEN_WIDTH, SCREEN_HEIGHT))
         return 1;
-    zText test(&wSDLMain, "Test text :3", 5, 5, "yellow", "Amstrad CPC", 16);
-    zText test2(&wSDLMain, "nyakomode go brrrrrrrrrrrrrrrrrrr", 5, 25, "yellow", "Amstrad CPC", 16);
-    zText test3(&wSDLMain, "$0000:", 5, 45, "yellow", "Amstrad CPC", 16);
-    zText reg_a(&wSDLMain, "A: ", REGISTER_X_OFFSET, 5, "yellow", "Amstrad CPC", 16);
-    zText reg_a_contents(&wSDLMain, toHex(gb.cpu.a_reg), reg_a.getX() + 32, reg_a.getY(), "yellow", "Amstrad CPC", 16);
+    zText test(&wSDLMain, "Test text :3", 8, 8, "yellow", "Amstrad CPC", 16);
+    zText test2(&wSDLMain, "nyahko go brrrrrrrrrrrrrrrrrrr", 8, 28, "yellow", "Amstrad CPC", 16);
+    zText test3(&wSDLMain, "$0000:", 8, 48, "yellow", "Amstrad CPC", 16);
+    zText reg_a(&wSDLMain, "A :", REGISTER_X_OFFSET, 8, "yellow", "Amstrad CPC", 16);
+    zText reg_a_value(&wSDLMain, toHex(gb.cpu.a_reg), reg_a.getX() + 48, reg_a.getY(), "yellow", "Amstrad CPC", 16);
+    zText pc_text(&wSDLMain, "PC:", REGISTER_X_OFFSET, 28, "yellow", "Amstrad CPC", 16);
+    zText pc_value(&wSDLMain, toHex(gb.cpu.pc), pc_text.getX() + 48, pc_text.getY(), "yellow", "Amstrad CPC", 16);
     // Main loop
     while(!quit) {
         // Only run if there are events on the queue
         while(wSDLMain.pollEvent() != 0) {
             // User requests quit
-            if(wSDLMain.eventHandler.type == SDL_QUIT)
+            if(wSDLMain.eventHandler.type == SDL_QUIT) {
                 quit = true;
+                break;
+            }
+            if(wSDLMain.eventHandler.type == SDL_KEYDOWN) {
+                switch(wSDLMain.eventHandler.key.keysym.sym) {
+                    case SDLK_SPACE:
+                        gb.cpu.pc++;
+                        break;
+                    case SDLK_RETURN:
+                        gb.cpu.pc += 0x100;
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
         // Run after we check for events
         // Need a clear screen function
         wSDLMain.clearScreen();
+        pc_value.updateText(toHex(gb.cpu.pc));
         wSDLMain.renderText();
         //test.render(5, 5);
         //test2.render();
