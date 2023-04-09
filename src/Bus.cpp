d0885f0f5329f2ab66fd6bdee75a1eb1cca4ec06
zeefu <hazedzephyr@gmail.com>
2023-04-09 12:42:53 -0700

    Add cart priority for cpu_r/w functions.

--- src/Bus.cpp
+++ src/Bus.cpp
@@ -9,11 +9,16 @@ Bus::Bus() {
 }
 
 void Bus::cpu_write(uint16_t addr, uint8_t data) {
+    // Check if the cartridge should handle this write
+    if (cart->cpu_write(addr, data)) {
+
+    }
     // Check if the passed address is for WRAM
-    if(addr >= 0xc000 && addr <= 0xfdff)
+    else if(addr >= 0xc000 && addr <= 0xfdff) {
         // Apply the address mask to the address so the WRAM can be "mirrored"
         // and so the offset of the passed address is 0x0000
         wram[addr & 0x1fff] = data;
+    }
     // Check if the passed address is for VRAM
     else if (addr >= 0x8000 && addr <= 0x9fff) {
         // Mask the passed address so it is offset to 0x0000
@@ -25,19 +30,23 @@ void Bus::cpu_write(uint16_t addr, uint8_t data) {
 }
 
 uint8_t Bus::cpu_read(uint16_t addr, bool read_only) {
-        // Check if the passed address is for WRAM
-        if(addr >= 0xc000 && addr <= 0xfdff)
-            // Apply the address mask to the address so the WRAM can be "mirrored"
-            // and so the offset of the passed address is 0x0000
-            return wram[addr & 0x1fff] ;
-        // Check if the passed address is for VRAM
-        else if (addr >= 0x8000 && addr <= 0x9fff) {
-            // Mask the passed address so it is offset to 0x0000
-            return ppu.cpu_read(addr & 0x1fff, read_only);
-        }
+    uint8_t data = 0x00;
+    if(cart->cpu_read(addr, data)) {
 
-        // If the address in not valid, return 0
-        return 0x00;
+    }
+    // Check if the passed address is for WRAM
+    else if(addr >= 0xc000 && addr <= 0xfdff) {
+        // Apply the address mask to the address so the WRAM can be "mirrored"
+        // and so the offset of the passed address is 0x0000
+        data = wram[addr & 0x1fff];
+    }
+    // Check if the passed address is for VRAM
+    else if (addr >= 0x8000 && addr <= 0x9fff) {
+        // Mask the passed address so it is offset to 0x0000
+        data = ppu.cpu_read(addr & 0x1fff, read_only);
+    }
+    // If the address in not valid, return 0
+    return data;
 }
 
 void Bus::reset() {
